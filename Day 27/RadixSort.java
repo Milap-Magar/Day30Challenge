7541cf696ce58a6dcaa480e5e474ac1ba62e5216
milap-magar <milapmagar12@gmail.com>
2024-07-30 20:46:51 +0545

    Day 26

---
+++ Day 27/RadixSort.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+public class RadixSort {
+
+    // A utility function to get maximum value in arr[]
+    static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    // A function to do counting sort of arr[] according to the digit represented by exp.
+    static void countSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n]; // output array
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        // Store count of occurrences in count[]
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        // Change count[i] so that count[i] now contains the actual position of this digit in output[]
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build the output array
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now contains sorted numbers according to the current digit
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    // The main function to that sorts arr[] of size n using Radix Sort
+    static void radixSort(int[] arr) {
+        // Find the maximum number to know the number of digits
+        int max = getMax(arr);
+
+        // Do counting sort for every digit. Note that instead of passing the digit number, exp is passed. exp is 10^i where i is the current digit number
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, exp);
+        }
+    }
+
+    // A utility function to print an array
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        radixSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
