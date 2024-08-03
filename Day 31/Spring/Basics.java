02b24817695b568679fc559c937cad04d30804f5
milap-magar <milapmagar12@gmail.com>
2024-08-03 21:13:04 +0545

    day 31

---
+++ Day 31/Spring/Basics.java
@@ -0,0 +1,10 @@
+package Basics.Spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basics {
+        public static void main(String[] args) {
+            SpringApplication.run(DemoApplication.class, args);
+        }
+}
